966efc48ed3ef5cb197486c1669ca83bb1030583
yeshwanth7407-spec <yeshwanth7407@gmail.com>
2025-11-20 06:47:10 +0530

    From assignment

--- DAY2/day2.cpp
+++ DAY2/day2.cpp
@@ -1,1 +1,16 @@
-sol1
+#include <iostream>
+using namespace std;
+
+int power(int a, int b){
+    int power = 1;
+    while (b--){
+        power *= a;
+    }
+    cout << power << endl;
+}
+
+int main(){
+    power(5, 2);
+    
+    return 0;
+}
