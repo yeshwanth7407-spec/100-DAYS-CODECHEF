519966601d2b5fc2b81a1c949150cc17eafbe198
yeshwanth7407-spec <yeshwanth7407@gmail.com>
2025-11-20 06:59:51 +0530

    assignment day3

---
+++ DAY3/day3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+void count(int n){
+    if(n>0){
+        int count=0;
+        while(n!=1){
+            if(n%2==0){
+                n/=2;
+            }else{
+                n=(n*3)+1;
+            }
+            count++;
+        }
+        cout<<count<<endl;
+    }
+}
+
+int main(){
+    count(34);
+}
